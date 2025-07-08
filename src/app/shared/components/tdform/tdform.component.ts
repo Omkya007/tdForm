aea934c47cf6a9c1835f868c823c274308e83733
Omkya007 <157211583+Omkya007@users.noreply.github.com>
2025-07-08 10:54:28 +0530

    first

---
+++ src/app/shared/components/tdform/tdform.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-tdform',
+  templateUrl: './tdform.component.html',
+  styleUrls: ['./tdform.component.scss']
+})
+export class TdformComponent implements OnInit {
+
+
+  @ViewChild('signUp')signUp!:NgForm
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+
+    console.log(this.signUp.value);
+    this.signUp.reset()
+    
+}
+
+}
